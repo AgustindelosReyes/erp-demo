1faa0267121dd1880ef6a1075350b4602a50581a
pieropacella <pieropaccc@gmail.com>
2025-11-25 18:04:18 -0300

    feat(frontend): agregar frontend al repositorio del ERP

---
+++ frontend/.next/dev/static/chunks/frontend_app_login_page_tsx_314666d3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/9e883_d0e53f86._.js",
+  "static/chunks/frontend_9589753f._.js"
+],
+    source: "dynamic"
+});
--- frontend/src/app/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://mi-backend-produccion.com/api'
-// };
